9b47f76ce2adad8815f68e2501588a0d2e245976
Jamieson.Smith <Jamie.smith@wonderlic.com>
2024-04-19 03:13:19 -0500

    Refactor config.js to include mailgunDomains

--- config.js
+++ config.js
@@ -2,16 +2,21 @@ const defaults = {
   PORT: '25',
 };
 
-function getEnvVar(key) { return process.env[key] || defaults[key]; }
-function getIntEnvVar(key) { return parseInt(getEnvVar(key)); }
-function getBoolEnvVar(key) { return getEnvVar(key) === 'true'; }
+function getEnvVar(key) {
+  return process.env[key] || defaults[key];
+}
+function getIntEnvVar(key) {
+  return parseInt(getEnvVar(key));
+}
+function getBoolEnvVar(key) {
+  return getEnvVar(key) === 'true';
+}
 
 const config = {
   port: getIntEnvVar('PORT'),
   mailgunAPIKey: getEnvVar('MAILGUN_API_KEY'),
   mailgunDomain: getEnvVar('MAILGUN_DOMAIN'),
+  mailgunDomains: getEnvVar('MAILGUN_DOMAINS'),
 };
-
-console.log(`config: ${JSON.stringify(config, null, ' ')}`);
 
 module.exports = config;
